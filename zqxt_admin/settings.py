1e5a87142ef70504f0c8348c9a88fb0639af9c34
chenxinsi <chenxinsi@hymost.com>
2017-08-15 15:29:30 +0800

    add blog static

--- zqxt_admin/settings.py
+++ zqxt_admin/settings.py
@@ -125,5 +125,5 @@ STATICFILES_DIRS = (
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-os.path.join(BASE_DIR, 'static'),
+os.path.join(BASE_DIR, 'blog/static'),
 )
